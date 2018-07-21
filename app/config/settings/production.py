66cb2fd728f2c17c89ef40eafad6086d3c6ad13b
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-21 18:15:58 +0900

    [UPDATE] S3 사용을 위한 settings.production 설정 추가 #1

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -15,6 +15,22 @@ else:
     DEBUG = False
     ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
+INSTALLED_APPS += [
+    'storages',
+]
+
+# AWS
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_DEFAULT_ACL = secrets['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = secrets['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = secrets['AWS_S3_SIGNATURE_VERSION']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+
+# Media
+DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
+STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
+
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
 DATABASES = secrets['DATABASES']
