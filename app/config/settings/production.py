84edef3337c02f4ed11e6d37a89aa8c5d04b6be2
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-21 18:30:01 +0900

    [UPDATE] Django 로그 기록 설정 추가 #1

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -34,3 +34,53 @@ STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
 DATABASES = secrets['DATABASES']
+
+# Log
+# /var/log/django 디렉토리가 존재하면 LOG_DIR로 그대로 사용
+# 없으면 ROOT_DIR/.log디렉토리를 사용 (없으면 생성)
+LOG_DIR = '/var/log/django'
+if not os.path.exists(LOG_DIR):
+    LOG_DIR = os.path.join(ROOT_DIR, '.log')
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+subprocess.call(['chmod', '755', LOG_DIR])
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
