f0333520f7c5537d4b8c98dcab780ea66f7391ea
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-31 20:40:22 +0900

    [REFACTOR] kakao_login 뷰 메소드 연결을 위한 import 설정 변경

--- app/members/urls/apis.py
+++ app/members/urls/apis.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .. import apis
+from ..apis import kakao
 
 urlpatterns = [
     path('', apis.UserList.as_view()),
     path('create/', apis.UserCreate.as_view()),
     path('<int:pk>/', apis.UserDetail.as_view()),
-    path('kakao/', apis.kakao.kakao_login, name='kakao-login'),
+    path('kakao/', kakao.kakao_login, name='kakao-login'),
 ]
