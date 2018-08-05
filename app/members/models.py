70d0d1339616f66714945bf2a6045bfc0010a54d
Ahyeon <ahyeoncode@gmail.com>
2018-08-05 22:46:16 +0900

    [FIX] user모델의 birthdatfield 재정의

--- app/members/models.py
+++ app/members/models.py
@@ -7,5 +7,5 @@ class User(AbstractUser):
     jibun_address = models.CharField(max_length=100, blank=True,)
     road_address = models.CharField(max_length=100, blank=True,)
     contact_phone = models.CharField(max_length=150, default="")
-    birthday = models.CharField(max_length=200, default="")
+    birthday = models.DateField(max_length=200, default="")
     # 초대코드
