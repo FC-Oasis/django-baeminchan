aed5cc33d63f8d6432f4ba711b10e651602857fa
HyungtaeMoon <blessmht@gmail.com>
2018-08-13 14:34:31 +0900

    [UPDATE] 유저 모델에 verbose_name, help_text 옵션 추가

--- app/members/models.py
+++ app/members/models.py
@@ -3,9 +3,23 @@ from django.db import models
 
 
 class User(AbstractUser):
-    fullname = models.CharField(verbose_name='user_name', max_length=100)
-    jibun_address = models.CharField(max_length=100, blank=True,)
-    road_address = models.CharField(max_length=100, blank=True,)
-    contact_phone = models.CharField(max_length=150, default="")
-    birthday = models.DateField(null=True, blank=True)
+    fullname = models.CharField(verbose_name='이름',
+                                max_length=100,
+                                help_text='필수입력')
+    jibun_address = models.CharField(max_length=100,
+                                     blank=True,
+                                     verbose_name='지번 주소',
+                                     help_text='선택입력')
+    road_address = models.CharField(max_length=100,
+                                    blank=True,
+                                    verbose_name='도로명 주소',
+                                    help_text='선택입력')
+    contact_phone = models.CharField(max_length=150,
+                                     default="",
+                                     verbose_name='전화번호',
+                                     help_text='필수입력, {3}-{4}-{4}형식만 허용')
+    birthday = models.DateField(null=True,
+                                blank=True,
+                                verbose_name='생년월일',
+                                help_text='필수입력')
     # 초대코드
