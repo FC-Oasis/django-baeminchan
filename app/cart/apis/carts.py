3c62de79ab76a3dcce80e61783c6391287926a3e
Ahyeon <ahyeoncode@gmail.com>
2018-08-15 05:33:09 +0900

    [ADD] UserCart API 구현 #22

--- app/cart/apis/carts.py
+++ app/cart/apis/carts.py
@@ -9,4 +9,13 @@ class CartList(generics.ListAPIView):
     serializer_class = CartSerializer
 
 
+class UserCart(generics.ListAPIView):
+    """
+    pk가 일치하는 user의 cart정보를 보여줌
+    """
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        queryset = Cart.objects.all()
+        return queryset.filter(user__pk=self.kwargs.get('pk'))
 
--- app/cart/urls/apis.py
+++ app/cart/urls/apis.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from ..apis.carts import CartList
+from ..apis.carts import CartList, UserCart
 
 urlpatterns = [
     path('list/', CartList.as_view()),
+    path('usercart/<int:pk>/', UserCart.as_view()),
 ]
