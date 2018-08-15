2c6ed0c4e2bc68b81f9816874e6a60dfbea2e234
Ahyeon <ahyeoncode@gmail.com>
2018-08-15 13:31:34 +0900

    [UPDATE] cart의 serializer 수정

--- app/cart/serializers/carts.py
+++ app/cart/serializers/carts.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from product.models import Product
 from ..models import Cart, CartItem
 from members.serializers import UserSerializer
+User = get_user_model()
 
 
 class ProductDetailForCartSerializer(serializers.ModelSerializer):
@@ -13,15 +15,25 @@ class ProductDetailForCartSerializer(serializers.ModelSerializer):
             'name',
             'sale_price',
         )
+        read_only_fields = ('sale_price', )
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     product = ProductDetailForCartSerializer()
+
     class Meta:
         model = CartItem
         fields = (
+            'pk',
             'product',
+            'amount',
+            'item_total_price',
         )
+
+    def update(self, instance, validated_data):
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.save()
+        return instance
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -36,4 +48,3 @@ class CartSerializer(serializers.ModelSerializer):
             'total_price',
         )
         read_only_fields = ('user', )
-
