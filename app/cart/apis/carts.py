fb0bafc9cd351acbd8753beb702ca9eefad9a381
Ahyeon <ahyeoncode@gmail.com>
2018-08-15 13:33:39 +0900

    [ADD] UserCartItemList,Detail API구현 #23

--- app/cart/apis/carts.py
+++ app/cart/apis/carts.py
@@ -24,3 +24,50 @@ class UserCart(generics.RetrieveUpdateAPIView):
         queryset = Cart.objects.all()
         return queryset.filter(user__pk=self.kwargs.get('pk'))
 
+
+class UserCartItemList(generics.ListCreateAPIView):
+    """
+    pk가 일치하는 user의 cart에 담긴 아이템리스트를 보여줌
+    """
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        queryset = CartItem.objects.all()
+        user_pk = self.kwargs.get('user_pk')
+        user = get_object_or_404(User, id=user_pk)
+        cart = get_object_or_404(
+            Cart,
+            user=user,
+        )
+        return queryset.filter(cart=cart)
+
+    def post(self, request, *args, **kwargs):
+        user_pk = self.kwargs.get('user_pk')
+        user = get_object_or_404(User, id=user_pk)
+        cart = get_object_or_404(
+            Cart,
+            user=user,
+        )
+        product_name = request.data.get('product.name')
+        product = Product.objects.get(name=product_name)
+        amount = request.data.get('amount')
+        cartitem = CartItem.objects.create(cart=cart, product=product, amount=amount)
+        serializer = CartItemSerializer(cartitem)
+        return Response(serializer.data)
+
+
+class UserCartItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    pk가 일치하는 user의 cart에 담긴 아이템을 보여줌
+    """
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        queryset = CartItem.objects.all()
+        user_pk = self.kwargs.get('user_pk')
+        user = get_object_or_404(User, id=user_pk)
+        cart = get_object_or_404(
+            Cart,
+            user=user,
+        )
+        return queryset.filter(cart=cart)
--- app/cart/urls/apis.py
+++ app/cart/urls/apis.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from ..apis.carts import CartList, UserCart
+from ..apis.carts import CartList, UserCart, UserCartItemList, UserCartItemDetail
 
 urlpatterns = [
     path('list/', CartList.as_view()),
     path('usercart/<int:pk>/', UserCart.as_view()),
+    path('cartitemlist/<int:user_pk>/', UserCartItemList.as_view()),
+    path('cartitemdetail/<int:user_pk>/<int:pk>/', UserCartItemDetail.as_view()),
 ]
