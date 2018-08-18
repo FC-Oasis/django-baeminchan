ce260ef1dd1457a40b250948a78e940b435c8f11
HyungtaeMoon <blessmht@gmail.com>
2018-08-18 20:11:30 +0900

    [ADD] CORS 설정 추가

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -37,6 +37,11 @@ AUTHENTICATION_BACKENDS = [
     'members.backends.KakaoBackend',
 ]
 
+# CORS
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+)
+
 
 # DRF
 REST_FRAMEWORK = {
@@ -59,9 +64,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
