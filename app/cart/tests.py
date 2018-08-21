e3e96123e33e4208bd455409f5d079f7e94fb377
Ahyeon <ahyeoncode@gmail.com>
2018-08-21 16:03:33 +0900

    [ADD] CartItem, UserCart API 테스트코드 추가

--- app/cart/tests.py
+++ app/cart/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from product.models import Product
+
+User = get_user_model()
+
+
+class CartItemTest(APITestCase):
+    URL = '/api/carts/cartitemlist/'
+
+    def tests_cart_item_create(self):
+        user = User.objects.create_user(username='dummy')
+        product = Product.objects.create(
+            raw_name='raw_name',
+            name='name',
+            price=100,
+            materials='materials',
+            alert_allergy='alert_allergy',
+            stock=10,
+        )
+        data = {
+            'product': product.pk,
+            'amount': 5,
+        }
+        self.client.force_authenticate(user=user)
+        response = self.client.post(self.URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class UserCartGetTest(APITestCase):
+    URL = 'api/carts/usercart/'
+
+    def test_cart_create(self):
+        user = User.objects.create_user(username='dummy')
+        self.client.force_authenticate(user=user)
+        response = self.client.get(self.URL)
+        print(response)
