3badb450e18f517ac4e4e9ae2d79f93ba3ae284b
Yeojin Kim <yeojin.dev@gmail.com>
2018-08-22 15:55:39 +0900

    [ADD] Comment 앱 모델링 #18

--- app/comment/models.py
+++ app/comment/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from product.models import Product
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    content = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+    rating = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0),
+        ],
+    )
+    created_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return f'{self.product.name}: {self.content}, rating: {self.rating} by {self.user}'
+
+    class Meta:
+        ordering = ('-created_at',)
