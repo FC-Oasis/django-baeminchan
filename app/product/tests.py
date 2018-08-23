e051a83587941990313a2136c4a8c4cc8435d987
Yeojin Kim <yeojin.dev@gmail.com>
2018-08-23 19:08:51 +0900

    [ADD] ProductRandom 테스트 코드 추가

--- app/product/tests.py
+++ app/product/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProductTest(APITestCase):
+    URL = '/api/products/'
+
+    def test_product_random_status_code(self):
+        response = self.client.get(
+            self.URL + 'random/',
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.post(
+            self.URL + 'random/'
+        )
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
