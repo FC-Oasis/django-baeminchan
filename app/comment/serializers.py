590bb9d42e4bf17c2e406913efb3ab7361271033
Yeojin Kim <yeojin.dev@gmail.com>
2018-08-24 00:06:28 +0900

    Revert "[UPDATE] Comment API에서 User 정보 표시 기능 추가"
    This reverts commit 8e2da6056c8bf8a34118c7dc9b0a6d85a5f53a8c.

--- app/comment/serializers.py
+++ app/comment/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
 
-from members.serializers import UserSimpleSerializer
 from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = UserSimpleSerializer()
 
     class Meta:
         model = Comment
